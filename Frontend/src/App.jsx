9924cb4ba05a1805af7e0146b6ef2bb9fba037fa
Sajit <sajitmaharjan@bibhutisolutions.com>
2025-08-28 17:42:40 +0545

    wip: student and admin access

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
-import StudentDashboard from "./pages/StudentDashboard";
 import Overview from "./pages/Overview";
 import CreateTask from "./pages/CreateTask";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "./pages/AdminDashboard";
+import InProgressList from "./pages/InProgressList";
+// import InProgressList from "./pages/InProgressList";
+import StudentDashboard from "./pages/StudentDashboard";
+import ToDoList from "./pages/ToDoList";
+import CompleteList from "./pages/Complete";
 
 const App = () => {
   return (
@@ -18,8 +22,16 @@ const App = () => {
 
         {/* dashboard phase */}
         {/* <Route path="/admin/dashboard" element={<AdminDashboard/>}/> */}
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-
+        {/* student access */}
+        <Route path="/student/dashboard" element={<StudentDashboard />} >
+           <Route index element={<ToDoList/>}/>
+           <Route path="todolist" element={<ToDoList/>}/>
+           <Route path="inprogress" element={<InProgressList/>}/>
+           <Route path="complete" element={<CompleteList/>}/>
+        
+        </Route>
+          
+         {/* Admin access */}
         <Route path="/admin/dashboard" element={<AdminDashboard />}>
           <Route index element={<Overview />} /> 
           <Route path="overview" element={<Overview />} />
